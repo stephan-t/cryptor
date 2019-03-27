db57befa06e7c9f4cbf179bfbd35d2f5afd93067
stephan-t <40370954+stephan-t@users.noreply.github.com>
2019-03-27 12:13:38 -0400

    Add option to use command prompts

--- src/cryptor/Main.java
+++ src/cryptor/Main.java
@@ -2,6 +2,7 @@ package cryptor;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Scanner;
 
 /**
  * Main entry point for running the Cryptor program.
@@ -10,6 +11,7 @@ public class Main {
 
     /**
      * Calls an encryption or decryption method according to command-line arguments.
+     * If no arguments are given then command prompts will be shown.
      * @param args the command-line arguments.<br>
      *             Arguments for encryption:
      *             <ol>
@@ -29,17 +31,40 @@ public class Main {
     public static void main(String[] args)
             throws IOException, GeneralSecurityException {
 
-        String mode = args[0];
-        String inFile = args[1];
-        String outFile = args[2];
-        char[] password = args[3].toCharArray();
+        if (args.length > 0) {
+            String mode = args[0];
+            String inFile = args[1];
+            String outFile = args[2];
+            char[] password = args[3].toCharArray();
 
-        if (mode.equals("-encrypt")) {
-            Cryptor.encrypt(inFile, outFile, password);
-        } else if (mode.equals("-decrypt")) {
-            Cryptor.decrypt(inFile, outFile, password);
+            if (mode.equals("-encrypt")) {
+                Cryptor.encrypt(inFile, outFile, password);
+            } else if (mode.equals("-decrypt")) {
+                Cryptor.decrypt(inFile, outFile, password);
+            } else {
+                throw new GeneralSecurityException("Not a valid cipher mode");
+            }
         } else {
-            throw new GeneralSecurityException("Not a valid cipher mode");
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter input filename: ");
+            String fileIn = scan.nextLine();
+
+            System.out.print("Enter output filename: ");
+            String fileOut = scan.nextLine();
+
+            System.out.print("Enter password: ");
+            char[] password = scan.nextLine().toCharArray();
+
+            System.out.print("Enter cipher mode [encrypt | decrypt]: ");
+            String modeIn = scan.nextLine();
+
+            if (modeIn.equals("encrypt")) {
+                Cryptor.encrypt(fileIn, fileOut, password);
+            } else if (modeIn.equals("decrypt")) {
+                Cryptor.decrypt(fileIn, fileOut, password);
+            } else {
+                throw new GeneralSecurityException("Not a valid cipher mode");
+            }
         }
     }
 }
